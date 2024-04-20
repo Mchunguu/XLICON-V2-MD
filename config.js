8efcc8b6823ec56e8cd9535ea67c6022438a851d
Mchunguu <153028670+Mchunguu@users.noreply.github.com>
2024-04-20 09:27:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.owner = [
   
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "" //0115876203
 
 global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
 global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
